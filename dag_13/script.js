8a8a04ee06574a8cedbd4c057614fbaa7215af2a
Juliekodehode <julie.kodehode@gmail.com>
2024-03-21 09:10:38 +0100

    Dag 13

---
+++ dag_13/script.js
@@ -0,0 +1,23 @@
+// .log() er en metode
+// Setningen i () er en string
+console.log("Hi planet earth!");
+
+// Variabler
+
+// let kan endre verdi
+// datatype: number
+let age = 26;
+console.log(typeof age)
+
+// const kan IKKE endre verdi
+// datatype: string
+const firstName = "Julie";
+console.log(typeof firstName)
+
+// datatype: boolean
+let isOnline = true;
+console.log(typeof isOnline)
+
+// kombinasjon av variablene vi har laget over
+const sentence = age + " " + firstName + " " + isOnline;
+console.log(sentence)
