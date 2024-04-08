96f18acd9b8dee9fc21625450a3c4a24b4fe1bfb
OysteinKoder <oystein.kodehode@gmail.com>
2024-04-08 13:21:49 +0200

    Refactor code to improve performance and readability

--- uke_7_loops/index.js
+++ uke_7_loops/index.js
@@ -1,3 +1,5 @@
+// For loop
+
 // When i (index) is smaller than 10 it launches the following code
 // i gets incremented by 1 (i++)
 // then the body of the function runs
@@ -7,6 +9,20 @@
 //
 // when i reaches 10 the code stops running, and the loop is completed
 
-for (i = 0; i <= 10; i++) {
-  console.log(i);
+const friendList = ["Per", "Pål", "Espen"];
+
+for (i = 0; i < friendList.length; i++) {
+  console.log(friendList[i]);
+}
+
+// DEL 1
+
+const cardContainer = document.querySelector("#card");
+
+for (i = 0; i < friendList.length; i++) {
+  const h2Title = document.createElement("h2");
+  const img = document.createElement("img");
+  img.src = h2Title.textContent = friendList[i];
+  console.log(h2Title);
+  cardContainer.appendChild(h2Title);
 }
