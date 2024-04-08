e15574197a1a1a8420c7917bd08e83f1382d61e0
OysteinKoder <oystein.kodehode@gmail.com>
2024-04-08 10:34:42 +0200

    Add comments explaining the loop behavior

--- uke_7_loops/index.js
+++ uke_7_loops/index.js
@@ -1,4 +1,11 @@
-console.log("hello");
+// When i (index) is smaller than 10 it launches the following code
+// i gets incremented by 1 (i++)
+// then the body of the function runs
+//  {
+//
+// }
+//
+// when i reaches 10 the code stops running, and the loop is completed
 
 for (i = 0; i <= 10; i++) {
   console.log(i);
